8215e116f05cc2cb7ab29da36009191d5264ec27
Elliot Murrey <elliotmurrey@gmail.com>
2016-06-07 13:12:51 -0600

    footer done

--- public_html/epic/homepage.php
+++ public_html/epic/homepage.php
@@ -51,7 +51,7 @@
 									<span class="icon-bar"></span>
 									<span class="icon-bar"></span>
 								</button>
-								<a class="navbar-brand" href="#">ROMuLess</a>
+								<a class="navbar-brand" href="#">.:ROMuLess:.</a>
 							</div>
 
 							<!-- Collect the nav links, forms, and other content for toggling -->
