a0499abcad73b5f7dc87fcc28ff2301d0a87cf1a
Don Deleeuw <donald.deleeuw@gmail.com>
2016-06-10 11:03:34 -0600

    Purchase/PayPal API draft update and save point - https://app.asana.com/0/117435671931140/118836678397128

--- public_html/paypal/account.php
+++ public_html/paypal/account.php
@@ -9,53 +9,35 @@ setXsrfCookie();
 <?php require_once("../php/partials/head-utils.php"); ?>
 <?php require_once("../php/partials/header.php"); ?>
 <link rel="stylesheet" href="../css/style.css" type="text/css">
-
-
-<!------------------------------------------------------->
-<!-- @author Donald Deleeuw <donald.deleeuw@gmail.com> -->
-<!------------------------------------------------------->
-
 <?php
-
 // ---------------------------------------- encrypted config files -------------------------------------
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 $config = readConfig("/etc/apache2/capstone-mysql/cartridge.ini");
 $paypal = json_decode($config["privkeys"])->paypal;
-
-
 // --------------------------------------------- get auth code -----------------------------------------
 require_once(dirname(__DIR__) . "../../vendor/autoload.php");
 $authCode = filter_input(INPUT_GET, "code", FILTER_SANITIZE_STRING);
-
-
 //------------------------------------------------ cURL ------------------------------------------------
 // ----- @see https://developer.paypal.com/docs/api/#identity
 // ----- @see http://incarnate.github.io/curl-to-php/
 // ----- cURL - initialize session - get access token from authorization code
 $ch = curl_init();
-
-
 // ----- cURL - set options
 curl_setopt($ch, CURLOPT_URL, "https://api.sandbox.paypal.com/v1/identity/openidconnect/tokenservice");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "client_id=" . $paypal->clientId . "&client_secret=" . $paypal->clientSecret . "&grant_type=authorization_code&code=" . $authCode);
 curl_setopt($ch, CURLOPT_POST, 1);
-
 // ----- cURL - get results
 $accessToken = curl_exec($ch);
-
 // ----- cURL - error checking
 if(curl_errno($ch)) {
 	echo 'Error:' . curl_error($ch);
 }
-
 // ----- cURL - close session
 curl_close($ch);
-
 // ------ break apart return JSON data in $accessToken
 $json = json_decode($accessToken);
 $accessTokenExtractToken = ($json->access_token);
-
 // ----- cURL - get user attributes
 // @see https://developer.paypal.com/docs/api/#get-user-information
 // @see http://incarnate.github.io/curl-to-php/
@@ -72,10 +54,8 @@ if(curl_errno($ch)) {
 	echo 'Error:' . curl_error($ch);
 }
 curl_close($ch);
-
 // ------ break apart return JSON data in $userAttributes
 $json = json_decode($userAttributes);
-
 //		$userAttributesUserId = ($json->user_id);
 $userAttributesName = ($json->name);
 $userAttributesEmail = ($json->email);
@@ -83,43 +63,24 @@ $userAttributesFirstName = ($json->given_name);
 $accountPpEmail = $userAttributesEmail;
 $accountName = $userAttributesName;
 $accountTempUserName = "TempUserName".rand(1, 9999999);
-
 //--------------------------------------------- mySQL -------------------------------------------------------
-
 require_once dirname(__DIR__) . "/php/classes/autoload.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
-
-
 use Edu\Cnm\CartridgeCoders;
 use Edu\Cnm\CartridgeCoders\Account;
-
-
-// verify the session, start if not active
-//if(session_status() !== PHP_SESSION_ACTIVE) {
-//	session_start();
-//}
-
 // grab the mySQL connection
 $pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cartridge.ini");
-
 // determine which HTTP method was used
 $method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
-
 //prepare an empty reply
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
-
-
 // ----------------------------------- mySQL lookup/new ----------------------------------
-
-
 // telling class this is a lookup
 $method = "GET";
-
 // Check if returning customer or new customer
 $accounts = CartridgeCoders\Account::getAccountByAccountPpEmail($pdo, $accountPpEmail);
-
 // -------- customer data does NOT exist, added new acount
 if (is_object($accounts) && (count(get_object_vars($accounts)) < 1)){
 	$account = new Account(null, 1, 1, 0, $accountName, $accountPpEmail, $accountTempUserName);
@@ -130,82 +91,18 @@ if (is_object($accounts) && (count(get_object_vars($accounts)) < 1)){
 
 	// update accountUserName with FirstName + accountId
 	$accountUserName = $userAttributesFirstName . $accountId;
-	var_dump($accountUserName); // ------------------------------------------------ DELETE ME
-
 	// replace temp username with personalized username
 	$account->setAccountUserName($accountUserName);
 	$account->update($pdo);
-
-
 } else {
 	// ---------- customer data already exist
 	// -- GET customer data
 	$account = Account::getAccountByAccountPpEmail($pdo, $accountPpEmail)[0];
 }
-
-
 ?>
-
-
-<!-- =============================== TEMP BATCHPAYOUT TESTING WITH $accessTokenExtractToken =======================-->
-
-<?php
-
-$ch = curl_init();
-
-$data = array(
-"sender_batch_id" => "batch_25", "email_subject" => "You have a payment","items" =>array("recipient_type" => "EMAIL", "amount" =>array("value" => "9.99", "curency" => "USD"),"receiver" => "cartridgecoders-buyer@gmail.com","note" => "Thank you.","sender_item_id" => "item_1"),array("value" => "1.99", "curency" => "USD"),"receiver" => "cartridgecoders-facilitator@gmail.com","note" => "Admin Fee.","sender_item_id" => "item_1");
-
-
-
-
-
-$data_string = json_encode($data);
-
-curl_setopt($ch, CURLOPT_URL, "https://api.sandbox.paypal.com/v1/payments/payouts/");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-'Content-Type: application/json',
-'Content-Length: ' . strlen($data_string))
-);
-
-$headers = array();
-//$headers[] = "Content-Type: application/json";
-//$headers[] = "Authorization: Bearer Access-Token";
-$headers[] = "Authorization: Bearer " . $accessTokenExtractToken;
-$headers[] = "Content-Type: application/json";
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-
-// ----- cURL - get results
-$result = curl_exec($ch);
-
-// ----- cURL - error checking
-if(curl_errno($ch)) {
-echo 'Error:' . curl_error($ch);
-}
-
-// ----- cURL - close session
-curl_close($ch);
-
-
-
-var_dump($data_string);
-var_dump($headers);
-var_dump($result);
-
-
-?>
-
-<!-- =============================== TEMP BATCHPAYOUT TESTING WITH $accessTokenExtractToken =======================-->
-
-
-
-
-
 <!--	----------------------------------------- Leave PHP/ Enter HTML -------------------------------------------->
+
+
 
 
 <div class="container">
@@ -220,24 +117,15 @@ var_dump($result);
 			<p>UserName: <?php echo $account->getAccountUserName(); ?></p>
 			<p></p>
 
+
+			GET ALL PRODUCTS BY PRODUCT-ACCOUNT-ID
+
+
+
+
 		</div>
 	</div>
 </div>
-
-
-<!--<div class="container">-->
-<!--	<div class="col-md12">-->
-<!--		<div class="well">-->
-<!--			<!-- PayPal Checkout Button begin -------------------------------------------------------------  -->
-<!--			<form action='expresscheckout.php' METHOD='POST'>-->
-<!--				<input type='image' name='submit' src='https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif'-->
-<!--						 border='0' align='top' alt='Check out with PayPal'/>-->
-<!--			</form>-->
-<!--			<!-- PayPal Checkout Button begin -------------------------------------------------------------  -->
-<!--		</div>-->
-<!--	</div>-->
-<!--</div>-->
-
 
 </body>
 </html>
---
+++ public_html/todo.php
@@ -0,0 +1,24 @@
+
+
+centralize - image angular
+
+centralize - EE's style
+
+footer
+-twitter
+-facetube
+-donate button
+
+account page ++ display user product by productaccountId
+
+form to create a new product
+
+form to send a message
+
+form leave feedback
+
+menu bar function
+search function
+
+
+
