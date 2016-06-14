a5cde783d85a89e9454a7ff34e4f6be751640544
Elliot Murrey <elliotmurrey@gmail.com>
2016-06-14 16:11:56 -0600

    making changes to the forms

--- public_html/angular/controllers/feedback-controller.js
+++ public_html/angular/controllers/feedback-controller.js
@@ -1,13 +1,13 @@
 app.controller("FeedbackController", ["$routeParams", "$scope", "FeedbackService", function($routeParams, $scope, FeedbackService) {
 
-	$scope.newFeedback = {feedbackId: null, fetchByFeedbackSenderId: null, feedbackProductId: null, feedbackRecipientId: null, feedbackContent: "", feedbackRating: ""};
+	$scope.newFeedback = {feedbackId: null, feedbackSenderId: "1", feedbackProductId: "10", feedbackRecipientId: "5", feedbackContent: "", feedbackRating: ""};
 	$scope.alerts = [];
 
 	/**
-	 * create feedback and sends it to the feedback API
+	 * create a feedback and sends to feedback api
 	 *
 	 * @param feedback to send
-	 * @param validated true if Angular validated the form, false if not
+	 * @param validated true if Angular validated the form, false if now
 	 **/
 
 	$scope.createFeedback = function(feedback, validated) {
@@ -16,7 +16,7 @@ app.controller("FeedbackController", ["$routeParams", "$scope", "FeedbackService
 				.then(function(result) {
 					if(result.data.status === 200) {
 						$scope.alerts[0] = {type: "success", msg: result.data.message};
-						$scope.newFeedback = {feedbackId: null, fetchByFeedbackSenderId: null, feedbackProductId: null, feedbackRecipientId: null, feedbackContent: "", feedbackRating: ""};
+						$scope.newFeedback = {feedbackId: null, feedbackSenderId: null, feedbackProductId: null, feedbackRecipientId: null, feedbackContent: "", feedbackSubject: ""};
 						$scope.addFeedbackForm.$setPristine();
 						$scope.addFeedbackForm.$setUntouched();
 					} else {
