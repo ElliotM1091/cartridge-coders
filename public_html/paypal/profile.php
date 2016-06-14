31a8b820176b7fd545fa0efa2d5ff5d9602e8795
Don Deleeuw <donald.deleeuw@gmail.com>
2016-06-14 11:19:57 -0600

    draft update: - https://app.asana.com/0/117435671931140/118836678397128

--- public_html/paypal/profile.php
+++ public_html/paypal/profile.php
@@ -14,18 +14,55 @@
 				<p>Email: <?php echo $_SESSION["sessionaccountppemail"]; ?></p>
 				<p>UserName: <?php echo $_SESSION["sessionaccountusername"]; ?></p>
 				<p></p>
-				<p><br></p>
-				<p><a href=<?php session_destroy(); ?>><img src="../image/destroysessiondata.gif" width="100"
-																		  height="100"></img></a> <--- click me to destroy session data
-					(testing link only)</p>
-				<p><br></p>
-				<?php var_dump($_SESSION); ?>
+<!--				<p><br></p>-->
+<!--				<p><a href=--><?php //session_destroy(); ?><!-->
+<!--				<img src="../image/destroysessiondata.gif" width="100"-->
+<!--																		  height="100"></img></a> <--- click me to destroy session data-->
+<!--					(testing link only)</p>-->
+<!--				<p><br></p>-->
+<!--				--><?php //var_dump($_SESSION); ?>
+			</div>
+
+
+
+			<div class="panel profilecss">
+				<div class="row">
+					<div class="col-md-3"><img src="../image/cartridge/atari2600-cartridge.jpg" alt="atari2600-cartridge.jpg"
+														height="200" width="200"></div>
+					<div class="col-md-8"><strong>TITLE:</strong> Asteroids<br><br><strong>PRICE:</strong> 14.99<br><br><strong>SHIPPING:</strong> 2.99<br><br><strong>DESCRIPTION:</strong> asteroids for in greate condition wear have had it packaged for a long time!!</div>
+						<div class="col-md-1"><i class="fa fa-trash fa-2x"></i><br>Delete<br><br><i class="fa fa-pencil fa-2x"></i><br>Edit</div>
+				</div>
+			</div>
+
+
+			<div class="panel profilecss">
+				<div class="row">
+					<div class="col-md-3"><img src="../image/cartridge/atari2600-cartridge.jpg" alt="atari2600-cartridge.jpg"
+														height="200" width="200"></div>
+					<div class="col-md-8"><strong>TITLE:</strong> Space invaders<br><br>strong>PRICE:</strong> 21.99<br><br><strong>SHIPPING:</strong> 4.99<br><br><strong>DESCRIPTION:</strong>
+						You are a space ship who must destroy the invading enemy space ships</div>
+					<div class="col-md-1"><i class="fa fa-trash fa-2x"></i><br>Delete<br><br><i class="fa fa-pencil fa-2x"></i><br>Edit</div>
+				</div>
+			</div>
+
+
+			<div class="panel profilecss">
+				<div class="row">
+					<div class="col-md-3"><img src="../image/cartridge/nintendo-64-cartridge.jpg" alt="nintendo-64-cartridge.jpg"
+														height="200" width="200"></div>
+					<div class="col-md-8"><strong>TITLE:</strong> Mario Kart 64<br><br>strong>PRICE:</strong> 39.99<br><br><strong>SHIPPING:</strong> 2.99<br><br><strong>DESCRIPTION:</strong>
+						Kart racing game need I say more. It's in really good condition too!!</div>
+					<div class="col-md-1"><i class="fa fa-trash fa-2x"></i><br>Delete<br><br><i class="fa fa-pencil fa-2x"></i><br>Edit</div>
+				</div>
+			</div>
+
+
+
+
+			</div>
 			</div>
 		</div>
-
-
-		
-		<div ng-view></div>
+	</div>
 	</div>
 	<?php require_once("../php/partials/footer.php"); ?>
 </body>
