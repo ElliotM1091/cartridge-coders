9144342443708235c6774506f6430ae3480f1fe5
Elliot Murrey <elliotmurrey@gmail.com>
2016-06-14 05:16:31 -0600

    still making the controller for message

--- public_html/angular/controllers/message-controller.js
+++ public_html/angular/controllers/message-controller.js
@@ -1,3 +1,11 @@
 app.controller("MessageController", ["$routeParams", "$scope", "MessageService", function($routeParams, $scope, MessageService) {
-	
-}])
+
+	$scope.newMessage = {messageId: null, messageSenderId: null, messageProductId: null, messageRecipientId: null, messageContent: "", messageMailGunId: null, messageSubject: ""};
+	$scope.alerts = [];
+
+	/**
+	 * create a message and sends to message api
+	 *
+	 * 
+	 **/
+}]);
