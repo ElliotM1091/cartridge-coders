437f2e2f244c00d09393ff7876805d25921f8c6a
Elliot Murrey <elliotmurrey@gmail.com>
2016-06-15 13:12:31 -0600

    RL helped make a fixed Footer yay

--- public_html/php/partials/footer.php
+++ public_html/php/partials/footer.php
@@ -7,18 +7,17 @@
 			<!--footer links/content-->
 				<div class="col-md-6 visible-md-block visible-lg-block footer-brand">
 					<div>
-						<a rel="license" href="&lt;/a&gt;&#10;&#9;&#9;&#9;&#9;&#9;&#9;&#9;&#9;&#9;&#9;&#9;&lt;p&gt;&lt;small&gt;&lt;em&gt;All content is licensed under &lt;a rel=" license"="" http:="" www.apache.org="" licenses="" license-2.0"="" target="_blank">All source code
-						released under the terms of the Apache License.</a><p></p>
+
 					</div>
 				</div>
 				<div class="col-xs-12 col-md-6">
 					<div class="foot-links col-md-6"></div>
 					<div class="foot-links col-md-6">
 						<h4 class="footer-info">Take a look at this stuff!</h4>
-						<a href=""><i class="fa fa-twitter-square fa-3x" aria-hidden="true"> |</i></a>
-						<a href=""><i class="fa fa-facebook-square fa-3x" aria-hidden="true"> |</i></a>
-						<a href=""><i class="fa fa-github-square fa-3x" aria-hidden="true"> |</i></a>
-						<a href=""><i class="fa fa-google-plus-square fa-3x" aria-hidden="true"> </i></a>
+						<a href="https://twitter.com/ddROMuLess"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"> |</i></a>
+						<a href="https://www.facebook.com/profile.php?id=100012389449145"><i class="fa fa-facebook-square fa-3x" aria-hidden="true"> |</i></a>
+						<a href="https://github.com/ElliotM1091/cartridge-coders"><i class="fa fa-github-square fa-3x" aria-hidden="true"> |</i></a>
+						<a href="https://plus.google.com/u/1/100097329771808305680/posts"><i class="fa fa-google-plus-square fa-3x" aria-hidden="true"> </i></a>
 					</div>
 				</div>
 				<div class="col-xs-12 col-md-6">
