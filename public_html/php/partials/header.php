91560ab367c7f8468d17e1c6c8ed4689f9b80369
Don Deleeuw <donald.deleeuw@gmail.com>
2016-06-14 19:25:59 -0600

    draft update: - https://app.asana.com/0/117435671931140/118836678397128

--- public_html/php/partials/header.php
+++ public_html/php/partials/header.php
@@ -16,59 +16,37 @@ $PREFIX = str_repeat("../", 3);
 <!-- header -->
 <header class="p-y-4">
 <!------------------------------------------------------------------------------------------------->
-	<!--if logged in "login" should be log out-->
-	<!--surpress create/send/feed back if not logged in-->
-	<!--nuke search-->
-
-	<!-- CHECK ACTIVE SESSION  -->
-<!--	--><?php
-//	if (isset($_SESSION["sessionaccountid"])) {
-//		echo "Session Active";
-//	} else {
-//		echo "Session Inactive";
-//	}
-//	?>
-	<!-- CHECK ACTIVE SESSION  -->
-
-
 	<div class="container">
+		<div class="panel topbarcss">
+			<div class="row">
+				<div class="col-md-5">ROMuLess</div>
 
-		<!-- brand and toggle  stuff-->
-		<nav class="navbar navbar-inverse">
-			<div class="container-fluid">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<a class="navbar-brand" href="index.html">ROMuLess</a>
 
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<form class="navbar-form navbar-left" role="search">
-						<div class="form-group">
-							<input type="text" size="15" class="form-control" placeholder="">
-						</div>
-						<button type="submit" class="btn btn-default">Search</button>
-					</form>
-<!--					other areas we can access-->
 
-					<div class="collapse navbar-collapse" id="main-nav">
-						<ul class="nav navbar-nav navbar-center">
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Product <span class="caret"></span></a>
-								<ul class="dropdown-menu">
-									<li><a href="createProduct">Add Product</a></li>
-									<li><a href="#">Another action</a></li>
-								</ul>
-							<li><a href="createProduct">Create Product</a></li>
-							<li><a href="sendMessage">Send Message</a></li>
-							<li><a href="sendFeedback">Feedback</a></li>
 
-							</ul>
-					</div>
-					<li class="nav navbar-nav navbar-right" ng-controller="PaypalController">
-						<a ng-click="loadPaypal();">Login&#160;&#160;&#160;<i class="fa fa-user fa-2x" aria-hidden="true"></i></a>
-					</li>
-				</div><!-- /.navbar-collapse -->
-			</div><!-- /.container-fluid -->
-		</nav>
+				<div class="col-md-5"><?php if (isset($_SESSION["sessionaccountid"])) {
+						echo "Logout";
+					} else {
+						echo "Login";
+					}
+					?>
+				</div>
+
+
+
+
+				<div class="col-md-2"><?php if (isset($_SESSION["sessionaccountid"])) {
+						echo "Logout";
+					} else {
+						echo "Login";
+					}
+					?>
+				</div>
+
+
+
+			</div>
+		</div>
 	</div>
 <!--	---------------------------------------------------------------------------------------------->
 
